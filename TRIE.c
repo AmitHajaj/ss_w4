3537ad796df5e63a7d195de48a853b3b98e05157
hajajon <amithajaj007@gmail.com>
2021-01-17 10:17:43 +0200

    printers

--- TRIE.c
+++ TRIE.c
@@ -161,14 +161,12 @@ int main(int argc, char *argv[])
         }
     }
 
-    int i;
-    for(i=0; i<argc; i++){
-        printf("argv[%d] = %s\n", i, argv[i]);
-    }
-
-    if(strcmp(argv[1], "r"))
+    if(argc > 1)
     {
-        print_words_reverse_inorder(root);
+        if(strcmp(argv[1], "r"))
+        {
+            print_words_reverse_inorder(root);
+        }
     }
     else
     {
